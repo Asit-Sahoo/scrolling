b0583091b6fa03288f5fc653315ba20c3fcab9ec
Asit Sahoo <110890707+Asit-Sahoo@users.noreply.github.com>
2023-12-27 17:11:28 +0530

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,10 @@
+function scrolll(){
+    var left=document.querySelector(".scroll-images");
+    left.scrollBy(350,0);
+
+}
+function scrollr(){
+    var right=document.querySelector(".scroll-images");
+    right.scrollBy(-350,0);
+    
+}
